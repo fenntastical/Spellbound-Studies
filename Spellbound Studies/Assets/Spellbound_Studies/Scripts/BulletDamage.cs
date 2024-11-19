8a929fd7803ed02af9abb6af4d505ae68cdc3b7b
Fenn <autumne@unr.edu>
2024-11-19 09:51:08 -0800

    working on bullets

---
+++ Spellbound Studies/Assets/Spellbound_Studies/Scripts/BulletDamage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    private PlayerHealth playerHealth;
+    private GameObject player;
+    public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        playerHealth.TakeDamage(damage);
+    }
+}
