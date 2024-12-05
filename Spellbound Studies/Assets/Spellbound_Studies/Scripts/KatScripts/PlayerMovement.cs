37369cc38f747fbf591b7ff45212f09afd929e66
Sofia Gratny <gratnysofia@gmail.com>
2024-12-04 16:12:51 -0800

    Revert "errors fixed (i hope)"
    This reverts commit b2547b08bf5824df0599c07d76110653c7fce0b7.

--- Spellbound Studies/Assets/Spellbound_Studies/Scripts/KatScripts/PlayerMovement.cs
+++ Spellbound Studies/Assets/Spellbound_Studies/Scripts/KatScripts/PlayerMovement.cs
@@ -3,50 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace KatScripts
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement : MonoBehaviour
+    [SerializeField] private float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 moveInput;
+    public Animator animator;
+    public Transform Aim;
+    private void Start()
     {
-        [SerializeField] private float moveSpeed = 5f;
-        public Rigidbody2D rb;
-        private Vector2 moveInput;
-        public Animator animator;
-        public Transform Aim;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
-        private void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+        if (context.canceled)
         {
-            rb = GetComponent<Rigidbody2D>();
-            animator = GetComponent<Animator>();
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("Last_Move_X", moveInput.x);
+            animator.SetFloat("Last_Move_Y", moveInput.y);
+            Vector3 vector3 = Vector3.left * moveInput.x + Vector3.down * moveInput.y;
+            Aim.rotation = Quaternion.LookRotation(Vector3.forward, vector3);
         }
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("Horizontal", moveInput.x);
+        animator.SetFloat("Vertical", moveInput.y);
 
-        // Update is called once per frame
-        void Update()
+        if (moveInput.x != 0 || moveInput.y != 0)
         {
-            rb.velocity = moveInput * moveSpeed;
+            Vector3 vector3 = Vector3.left * moveInput.x + Vector3.down * moveInput.y;
+            Aim.rotation = Quaternion.LookRotation(Vector3.forward, vector3);
+            animator.SetFloat("Last_Move_X", moveInput.x);
+            animator.SetFloat("Last_Move_Y", moveInput.y);
         }
+   
 
-        public void Move(InputAction.CallbackContext context)
-        {
-            animator.SetBool("isWalking", true);
-            if (context.canceled)
-            {
-                animator.SetBool("isWalking", false);
-                animator.SetFloat("Last_Move_X", moveInput.x);
-                animator.SetFloat("Last_Move_Y", moveInput.y);
-                Vector3 vector3 = Vector3.left * moveInput.x + Vector3.down * moveInput.y;
-                Aim.rotation = Quaternion.LookRotation(Vector3.forward, vector3);
-            }
-            moveInput = context.ReadValue<Vector2>();
-            animator.SetFloat("Horizontal", moveInput.x);
-            animator.SetFloat("Vertical", moveInput.y);
-
-            if (moveInput.x != 0 || moveInput.y != 0)
-            {
-                Vector3 vector3 = Vector3.left * moveInput.x + Vector3.down * moveInput.y;
-                Aim.rotation = Quaternion.LookRotation(Vector3.forward, vector3);
-                animator.SetFloat("Last_Move_X", moveInput.x);
-                animator.SetFloat("Last_Move_Y", moveInput.y);
-            }
-        }
     }
 }
