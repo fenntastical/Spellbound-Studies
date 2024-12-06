78887b544edebc58a468b6713f1379c18759825d
Kat <katw@unr.edu>
2024-12-06 05:13:21 -0800

    im giving up!

--- Spellbound Studies/Assets/playerCombat.cs
+++ Spellbound Studies/Assets/playerCombat.cs
@@ -7,7 +7,6 @@ public class playerCombat : MonoBehaviour
     public GameObject Melee;
     //bool isAttacking = false;
     public Animator animator;
-    //public Animator swordAnim;
     public Transform attackPoint;
     public LayerMask enemyLayers;
 
@@ -34,7 +33,6 @@ public class playerCombat : MonoBehaviour
     {
         //plays an attack animation
         animator.SetTrigger("Attack");
-        //swordAnim.SetTrigger("SwordAttack");
         //detects enemies within range of attack
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         //damages enemies
---
+++ Spellbound Studies/Assets/swordParent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordParent : MonoBehaviour
+{
+    public SpriteRenderer swordRenderer;
+    public Animator swordAnim;
+    private float timer;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (timer > .5)
+            {
+                timer = 0;
+                Attack();
+            }
+            // Attack();
+            // AudioMgr.Instance.PlaySFX("Attack");
+        }
+    }
+
+    public void Attack()
+    {
+        swordAnim.SetTrigger("SwordAttack");
+    }
+}
